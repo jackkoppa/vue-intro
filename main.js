29375c458d3dbbcaa2d89e83e926fde627e70c5d
Jack Koppa <hello@jackkoppa.com>
2018-07-25 19:43:59 -0400

    Display image using Vue url

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var app = new Vue({
     el: "#app",
     data: {
         product: 'Socks',
-        description: 'they\'re things that go on your feet'
+        image: './images/green_socks.jpeg'
     }
 })
